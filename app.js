4140dd744e9dcac3be68d92bed27c56cd4a59bf2
goatchild <40519554+goatchild@users.noreply.github.com>
2023-02-05 01:41:14 +0100

    1

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 const passportLocalMongoose = require("passport-local-mongoose");
-const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
 const express = require("express");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const LocalStrategy = require("passport-local");
-const User = require('./models/user');
- 
+const User = require("./models/user");
+
 var app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 });
-  
+
 UserSchema.plugin(passportLocalMongoose);
 
 passport.use(new LocalStrategy(User.authenticate()));
@@ -29,37 +29,41 @@ passport.deserializeUser(User.deserializeUser());
 module.exports = User;
 
 // middleware functions that use UserSchema and User go here
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Rusty is a dog",
     resave: false,
-    saveUninitialized: false
-}));
- 
-mongoose.connect("mongodb+srv://nawaf_bleyer:Tcndtggj3ZbfbwVmTfh5ZANPf2bJYZ@cluster0.ompgx4g.mongodb.net/test", {
+    saveUninitialized: false,
+  })
+);
+
+mongoose.connect(
+  "mongodb+srv://nawaf_bleyer:Tcndtggj3ZbfbwVmTfh5ZANPf2bJYZ@cluster0.ompgx4g.mongodb.net/test",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'views/css')));
-app.use(express.static('views'))
+app.use(express.static(path.join(__dirname, "views/css")));
+app.use(express.static("views"));
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 function isAdmin(req, res, next) {
-    if (req.isAuthenticated() && req.user.roles === 'admin') {
-        return res.redirect("/secret_admin");
-    } else {
-        return res.redirect("/secret");
-    }
+  if (req.isAuthenticated() && req.user.roles === "admin") {
+    return res.redirect("/secret_admin");
+  } else {
+    return res.redirect("/secret");
+  }
 }
-
 
 //=====================
 // ROUTES
@@ -67,121 +71,131 @@ function isAdmin(req, res, next) {
 
 // Showing home page
 app.get("/", function (req, res) {
-    res.render("home");
+  res.render("home");
 });
 
 // Showing secret page
 app.get("/secret", isLoggedIn, function (req, res) {
-    if (req.user.roles === 'admin') {
-        res.render("secret_admin");
-    } else if (req.user.roles === 'user'){
-        res.render("secret");
-    }
+  if (req.user.roles === "admin") {
+    res.render("secret_admin");
+  } else if (req.user.roles === "user") {
+    res.render("secret.ejs", { username: req.user.username });
+  }
 });
 
 // Showing register form
 app.get("/register", function (req, res) {
-    res.render("register");
+  res.render("register");
 });
 
 // Handling user signup
 app.post("/register", function (req, res) {
-    User.register({ username: req.body.username, roles: 'user' }, req.body.password, function (err, user) {
-        if (err) {
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function () {
-            res.redirect("/secret");
-        });
-    });
+  User.register(
+    { username: req.body.username, roles: "user" },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secret");
+      });
+    }
+  );
 });
 
 // Handling password change 1
 app.post("/changepassword", isLoggedIn, function (req, res) {
-    User.findOne({ username: req.user.username }, (err, user) => {
+  User.findOne({ username: req.user.username }, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.render("account", { error: "Error, please try again" });
+    }
+    if (!user) {
+      console.log("User not found");
+      return res.render("account", { error: "Error, please try again" });
+    }
+
+    user.setPassword(req.body.newpassword, (err) => {
+      if (err) {
+        console.log(err);
+        return res.render("account", { error: "Error, please try again" });
+      }
+      user.save((err) => {
         if (err) {
+          console.log(err);
+          return res.render("account", { error: "Error, please try again" });
+        }
+        req.logIn(user, (err) => {
+          if (err) {
             console.log(err);
             return res.render("account", { error: "Error, please try again" });
-        }
-        if (!user) {
-            console.log("User not found");
-            return res.render("account", { error: "Error, please try again" });
-        }
-
-        user.setPassword(req.body.newpassword, (err) => {
-            if (err) {
-                console.log(err);
-                return res.render("account", { error: "Error, please try again" });
-            }
-            user.save((err) => {
-                if (err) {
-                    console.log(err);
-                    return res.render("account", { error: "Error, please try again" });
-                }
-                req.logIn(user, (err) => {
-                    if (err) {
-                        console.log(err);
-                        return res.render("account", { error: "Error, please try again" });
-                    }
-                    //return res.render("account", { message: "Password changed successfully" });
-                    res.render("passwordChanged");
-                });
-            });
+          }
+          //return res.render("account", { message: "Password changed successfully" });
+          res.render("passwordChanged");
         });
+      });
     });
+  });
 });
 
 //Showing login form
 app.get("/login", function (req, res) {
-    res.render("login");
+  res.render("login");
 });
 
 //Handling user login
-app.post("/login", passport.authenticate("local", {
+app.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/secret",
-    failureRedirect: "/login"
-}), function (req, res) {
-});
+    failureRedirect: "/login",
+  }),
+  function (req, res) {}
+);
 
 //Handling user logout
 app.get("/logout", function (req, res) {
-    req.logout(function (err) {
-        if (err) {
-            console.log(err);
-        }
-        res.redirect("/");
-    });
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
 });
 
 //Handling account
 app.get("/account", isLoggedIn, function (req, res) {
-    res.render("account");
+  res.render("account");
 });
 
 //Handling Admins
 app.get("/secret_admin", isLoggedIn, isAdmin, function (req, res) {
-    if (req.user.roles === 'admin') {
-        return res.render("secret_admin");
-    } else {
-        res.redirect("/secret_admin");
-    }
+  if (req.user.roles === "admin") {
+    return res.render("secret_admin");
+  } else {
+    res.redirect("/secret_admin");
+  }
 });
 
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 //=====================
 // CONNECTION
 //=====================
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    res.redirect("/login");
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
 }
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-    console.log("Server Has Started!");
+  console.log("Server Has Started!");
 });
 
-mongoose.set('strictQuery', false);
-
+mongoose.set("strictQuery", false);
